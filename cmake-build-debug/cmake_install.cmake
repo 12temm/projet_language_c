c3d15b6b6096afeed8a4aee114f1b5aabd23dc26
eouthaithavye <eouthaithavye@gmail.com>
2025-11-20 16:48:58 +0100

    Changement nom

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/repo-projet-c
+# Install script for directory: C:/projet_langage_C
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/repo-projet-c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/projet_langage_C/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
