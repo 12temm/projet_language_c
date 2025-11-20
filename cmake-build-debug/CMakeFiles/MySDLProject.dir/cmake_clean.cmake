4b6e4c565c6e9e36316bc0935cf210f8dc1a5511
eouthaithavye <eouthaithavye@gmail.com>
2025-11-20 16:46:48 +0100

    Ajout map 1

---
+++ cmake-build-debug/CMakeFiles/MySDLProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySDLProject.dir/main.c.obj"
+  "CMakeFiles/MySDLProject.dir/main.c.obj.d"
+  "MySDLProject.exe"
+  "MySDLProject.exe.manifest"
+  "MySDLProject.pdb"
+  "libMySDLProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MySDLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
